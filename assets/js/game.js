44e597d0a240497fd0ba059cbc3d9fd5d8feb12d
eisforgene <82473195+eisforgene@users.noreply.github.com>
2022-01-21 11:15:47 -0800

    able to track player and robot's information

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,11 +1,19 @@
-let playerName = window.prompt("What is your robot's name?");
+let playerName = window.prompt('What is your robot\'s name?');
+let playerHealth = 100;
+let playerAttack = 10;
 
-console.log(playerName);
+console.log(playerName, playerAttack, playerHealth);
 
+let enemyName = 'Roborto';
+let enemyHealth = 50;
+let enemyAttack = 12;
 
-
-function fight() {
-    window.alert("The fight has begun!");
+let fight = () => {
+    window.alert('Welcome to Robot Gladiators');
 }
+
+// let fight = function() {
+//     window.alert('Welcome to Robot Gladiators');
+// };
 
 fight();
